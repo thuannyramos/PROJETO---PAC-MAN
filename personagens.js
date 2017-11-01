849841d5c72f0b8c99e122de29baca8fa1672a2a
thuannyramos <thuannyramosms@gmail.com>
2017-11-01 06:17:29 -0300

    Cenário, comidas do pac e pontuação.

--- personagens.js
+++ personagens.js
@@ -4,21 +4,26 @@ function creatPac(){
   if (keyIsDown(LEFT_ARROW)){
 		pac.changeAnimation("movingH");
 
+	if (!(colisao (pac.position.x-1-bloco/2, pac.position.y - 10) || colisao (pac.position.x-1-bloco/2, pac.position.y + 10))) {
 	  if(inScreen)
 		{
 		pac.position.x  -= 5;
 		pac.mirrorX(-1);
-	}
+	  }
 	  
 	  else {
 		pac.position.x = w - 40;
 		pac.mirrorX(1);
 		inScreen= true;
-	}	 
+	  }	
+	}  
+	colisaoComida(pac.position.x, pac.position.y);
 }
  if (keyIsDown(RIGHT_ARROW)){
 		pac.changeAnimation("movingH");
+	if (!(colisao(pac.position.x+0+bloco/2, pac.position.y - 10) || colisao(pac.position.x+0+bloco/2, pac.position.y + 10))) {
 		if(inScreen){
+			
 			pac.position.x +=5;
 			pac.mirrorX(1);
 		}
@@ -26,45 +31,54 @@ function creatPac(){
 			pac.position.x = 40;
 			pac.mirrorX(-1);
 			inScreen = true;
-		}
+	}	}
+	
+	colisaoComida(pac.position.x, pac.position.y);
   }
   
   if(pac.position.x < 0 || pac.position.x > w || pac.position.y < 0 || pac.position.y > h) {
 		inScreen = false;
   }
   
+    
   //Movimentação vertical - Pac
 	
    if (keyIsDown(UP_ARROW)){
 		pac.changeAnimation("movingV");
-		
-    if(inScreen){
+	
+	if (!( colisao(pac.position.x - 10 , pac.position.y-1-bloco/2) || colisao(pac.position.x + 10 , pac.position.y-1-bloco/2) )) {	
+      if(inScreen){
 			pac.position.y -= 5;
 			pac.mirrorY(1);
 		}
     
-    else{
+      else{
 			pac.position.y = h - 40;
 			pac.mirrorY(-1);
 		 	inScreen= true;
 		}
+	}		
+	colisaoComida(pac.position.x, pac.position.y);
 }
 
    if (keyIsDown(DOWN_ARROW)){
 		pac.changeAnimation("movingV");
 		
+	  if (!(colisao(pac.position.x - 10, pac.position.y+0+bloco/2) || colisao(pac.position.x + 10, pac.position.y+0+bloco/2))) {	
 		if(inScreen)
 		{
-			pac.position.y +=5;
+			pac.position.y += 5;
 			pac.mirrorY(-1);
 		}
     
-    else{
+       else{
 		 pac.position.y = 40;
 		 pac.mirrorY(-1);
 		 inScreen= true;
 		}
+	  }
 	}
+	colisaoComida(pac.position.x, pac.position.y);
  //else pac.changeAnimation("stopped");
 
 }
@@ -102,3 +116,25 @@ function creatPac(){
 	return pF;
 }
 
+function colisao (cx, cy) {
+  colColuna = Math.floor (cx/bloco);
+  colLinha = Math.floor (cy/bloco);
+  
+  
+  if (cenario [colLinha][colColuna] == 'b') {
+	  return true;
+  }
+  else {
+	  return false;
+  } 
+  
+}
+
+function colisaoComida (cx, cy){
+	colColuna = Math.floor (cx/bloco);
+   colLinha = Math.floor (cy/bloco);
+	if (cenario [colLinha][colColuna] == 'v') {
+	  cenario [colLinha][colColuna] = 'x';
+	 contadorPonto +=10;
+  } 
+}
